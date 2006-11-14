e7beed5014aad09e67cae89fb83d95f6a0778719
seninp <seninp@5003ad82-3f21-0410-b9e6-d97d5e13bc38>
2006-11-14 16:44:58 +0000

    moving from home

--- trunk/rcode/moves.R
+++ trunk/rcode/moves.R
@@ -25,3 +25,65 @@ segmentImage <- function(mu, sigma, raster){
  }
  segmentation
 }
+
+singleton <- function(raster_value, mean, variance)
+{
+  log(sqrt(2.0*3.141592653589793*variance))+
+    ((raster_value-mean)^2)/(2.0*variance)
+}
+
+
+doubleton <- function(i, j, label, raster, labels, beta)
+{
+  energy <- 0.0;
+  height <- dim(raster)[1]
+  widtht <- dim(raster)[2]
+
+  if (i!=height-1) # south
+    {
+      if (label == labels[i+1][j]) energy <- energy - beta
+      else energy <- energy + beta
+    }
+  if (j!=width-1) # east
+    {
+      if (label == labels[i][j+1]) energy <- energy - beta
+      else energy <- energy + beta
+    }
+  if (i!=0) # nord
+    {
+      if (label == labels[i-1][j]) energy <- energy - beta
+      else energy <- energy + beta
+    }
+  if (j!=0) # west
+    {
+      if (label == labels[i][j-1]) energy <- energy - beta
+      else energy <- energy + beta
+    }
+  energy;
+}
+
+local_energy <- function(i, j, raster, labels, classes, label)
+{
+  ## singleton singleton <- function(raster_value, mean, variance)
+  ## doubleton doubleton <- function(i, j, label, raster, labels, beta)
+  singleton(raster[i][j], classes[label][1], classes[label][2]) + 
+    doubleton(i,j,label,raster,labels,beta)
+}
+
+energy <- function(raster, labels, classes, beta)
+{
+  singletons <- 0.0;
+  doubletons <- 0.0;
+  height <- dim(raster)[1]
+  widtht <- dim(raster)[2]
+  
+  for (i in 1:height){
+    for (j in 1:width){
+	  k <- labels[i][j]
+	  ## singleton singleton <- function(raster_value, mean, variance)
+	  singletons <- singletons + singleton(raster[i][j], classes[k][1], classes[k][2])
+	  ## doubleton doubleton <- function(i, j, label, raster, labels, beta)
+	  doubletons <- doubletons + doubleton(i,j,k,raster,labels, beta)
+      }
+  singletons + doubletons/2; 
+}
